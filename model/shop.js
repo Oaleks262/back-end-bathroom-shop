7d95e7a114de5d31afad180a35e316e1e403e3ef
Oleksand-Zvirych <oaleks262@gmail.com>
2024-01-18 15:42:48 +0200

    edit

--- model/shop.js
+++ model/shop.js
@@ -37,7 +37,7 @@ const ShopSchema = new mongoose.Schema({
         required: true,
     },
     phoneNumber: {
-        type: Number,
+        type: String,
         required:true,
     },
     city: {
