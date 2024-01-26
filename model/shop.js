2ce07fa3f4115ff988d0980229cd22767b720c35
Oleksand-Zvirych <oaleks262@gmail.com>
2024-01-26 11:59:59 +0200

    edit

--- model/shop.js
+++ model/shop.js
@@ -5,7 +5,7 @@ const basketShopSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    item:{
+    item: {
         type: String,
         required: true,
     },
@@ -18,14 +18,18 @@ const basketShopSchema = new mongoose.Schema({
         required: true,
     },
     total: {
-        type: Number,
-        required: true,
-    }
+        type: Number
+    },
+});
+
+basketShopSchema.pre('save', function (next) {
+    // Перерахунок total для кожного товару у корзині перед збереженням
+    this.total = this.price * this.quantity;
+    next();
 });
 
 const ShopSchema = new mongoose.Schema({
-
-    firstName:{
+    firstName: {
         type: String,
         required: true,
     },
@@ -35,7 +39,7 @@ const ShopSchema = new mongoose.Schema({
     },
     phoneNumber: {
         type: String,
-        required:true,
+        required: true,
     },
     city: {
         type: String,
@@ -45,17 +49,27 @@ const ShopSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    numberPost:{
+    numberPost: {
         type: String,
         required: true,
     },
     productItems: {
-        type: [basketShopSchema], 
+        type: [basketShopSchema],
         required: true,
     },
     acrivePosition: {
         type: String,
-    }
+    },
+    totalAmount: {
+        type: Number,
+    },
+});
 
-})
+ShopSchema.pre('save', function (next) {
+    // Перерахунок загальної суми за всі продукти та товари у корзині перед збереженням
+    const totalAmount = this.productItems.reduce((total, item) => total + item.total, 0);
+    this.totalAmount = totalAmount;
+    next();
+});
+
 export default mongoose.model('Shop', ShopSchema);
